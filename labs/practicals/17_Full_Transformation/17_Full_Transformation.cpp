bbeb7d9544b1e423304b1bf8b47f920a713f10bd
Francis <40224069@live.napier.ac.uk>
2017-03-24 13:36:50 +0000

    Practical 17: Full Transformation

--- labs/practicals/17_Full_Transformation/17_Full_Transformation.cpp
+++ labs/practicals/17_Full_Transformation/17_Full_Transformation.cpp
@@ -74,11 +74,13 @@ bool render() {
   // *********************************
   // Create transformation matrices
   // ******************************
-
+  R = rotate(mat4(1.0f), theta, vec3(1.0f, -1.0f, 0.0f));
+  S = scale(mat4(1.0f), vec3(s, s, s));
+  T = translate(mat4(1.0f), pos);
 
 
   // Combine matrices to set M - remember multiplication order
-
+  M = T * (R * S);
   // *********************************
   // Create MVP matrix
   auto V = cam.get_view();
