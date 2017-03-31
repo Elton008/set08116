949677765aff6895939c6794562d47a7f1b7b60f
Francis <40224069@live.napier.ac.uk>
2017-03-31 12:01:05 +0100

    Practical 20: Transformed Cube

--- labs/practicals/20_Transformed_Cube/20_Transformed_Cube.cpp
+++ labs/practicals/20_Transformed_Cube/20_Transformed_Cube.cpp
@@ -20,23 +20,30 @@ bool load_content() {
       // *********************************
       // Add the position data for triangles here, (6 verts per side)
       // Front
-
+	  vec3(-1,1,0), vec3(1,-1,0), vec3(1,1,0),//Triangle 1 vectors
+	  vec3(-1,1,0), vec3(-1,-1,0), vec3(1,-1,0),//Triangle 2 vectors
 
       // Back
+	  vec3(-1,1,-1), vec3(1,-1,-1), vec3(1,1,-1),//Triangle 3 vectors
+	  vec3(-1,1,-1), vec3(-1,-1,-1), vec3(1,-1,-1),//Triangle 4 vectors
 
 
       // Right
-
+	  vec3(1,1,0), vec3(1,-1,-1), vec3(1,1,-1),//Triangle 5 vectors
+	  vec3(1,1,0), vec3(1,-1,0), vec3(1,-1,-1),//Triangle 6 vectors
 
       // Left
 
-
+	  vec3(-1,1,-1), vec3(-1,-1,0), vec3(-1,1,0),//Triangle 7 vectors
+	  vec3(-1,1,-1), vec3(-1,-1,-1), vec3(-1,-1,0),//Triangle 8 vectors
       // Top
 
-
+	  vec3(-1,1,-1), vec3(1,1,0), vec3(1,1,-1),//Triangle 9 vectors
+	  vec3(-1,1,-1), vec3(-1,1,0), vec3(1,1,0),//Triangle 10 vectors
       // Bottom
 
-
+	   vec3(-1,-1,0), vec3(1,-1,-1), vec3(1,-1,0),//Triangle 11 vectors
+	  vec3(-1,-1,0), vec3(-1,-1,-1), vec3(1,-1,-1),//Triangle 12 vectors
       // *********************************
   };
   // Colours
@@ -63,40 +70,36 @@ bool load_content() {
 }
 
 bool update(float delta_time) {
-  // *********************************
-  // Use keys to update transform values
-  // WSAD - movement
-  // Arrow Keys - rotation
-  // O decrease scale, P increase scale
+	// *********************************
+	// Use keys to update transform values
+	// WSAD - movement
+	// Arrow Keys - rotation
+	// O decrease scale, P increase scale
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	// Accumulate time
+	rho += delta_time;
+	// Update the scale - base on sin wave
+	s = 1.0f + sinf(rho);
+	// Multiply by 5
+	s *= 5.0f;
+	// Increment theta - half a rotation per second
+	theta += pi<float>() * delta_time;
+	// Check if key is pressed
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_UP)) {
+		pos += vec3(0.0f, 0.0f, -5.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_DOWN)) {
+		pos += vec3(0.0f, 0.0f, 5.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_LEFT)) {
+		pos += vec3(-5.0f, 0.0f, 0.0f) * delta_time;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_RIGHT)) {
+		pos += vec3(5.0f, 0.0f, 0.0f) * delta_time;
+	}
+		
 
   // *********************************
   // Update the camera
@@ -110,9 +113,11 @@ bool render() {
   mat4 T, R, S, M;
   // *********************************
   // Create transformation matrix
+  R = rotate(mat4(1.0f), theta, vec3(1.0f, -1.0f, 0.0f));
+  S = scale(mat4(1.0f), vec3(s, s, s));
+  T = translate(mat4(1.0f), pos);
 
-
-
+  M = T * (R * S);
 
   // *********************************
   // Create MVP matrix
